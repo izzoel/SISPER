034cdc05ecb0ffe05be7903c86ad5ea48e2e51ab
izzoel <ramadhani.zulfahmi@gmail.com>
2025-02-20 11:48:03 +0800

    - fix livewire template

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -120,7 +120,37 @@
     </div>
     <!-- / Layout wrapper -->
 
-    @include('auth.layout.footer')
+    <!-- Core JS -->
+    <script src="{{ asset('vendor/sneat/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('vendor/sneat/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('vendor/sneat/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('vendor/sneat/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('vendor/sneat/js/menu.js') }}"></script>
+
+    <!-- Vendors JS -->
+    <script src="{{ asset('vendor/sneat/libs/apex-charts/apexcharts.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ asset('vendor/sneat/js/main.js') }}"></script>
+
+    <!-- Popover JS -->
+    <script src="{{ asset('vendor/sneat/js/ui-popover.js') }}"></script>
+
+
+    <!-- Toast JS -->
+    <script src="{{ asset('vendor/sneat/js/ui-toasts.js') }}"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.1.8/b-3.2.0/b-html5-3.2.0/r-3.0.3/datatables.min.js"></script>
+
+
+    {{-- @include('auth.scripts.forpi') --}}
+    @include('auth.scripts.' . Route::currentRouteName())
+    {{-- @yield(Route::currentRouteName() ? Str::replace('.', '-', Route::currentRouteName()) : 'content') --}}
 
 </body>
 
