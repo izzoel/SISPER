d81efc1782a77a15fda473b416737aff01064a8e
izzoel <ramadhani.zulfahmi@gmail.com>
2025-03-02 23:45:20 +0800

    + adminonlymiddleware

---
+++ app/Http/Middleware/AdminOnlyMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminOnlyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            return $next($request);
+        }
+
+        // Jika pengguna tidak terautentikasi sebagai admin, tampilkan pesan kesalahan
+        return response()->json(['error' => 'Anda tidak memiliki izin untuk mengakses halaman ini.'], 403);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\Portal;
 use App\Http\Controllers\ForpiPisn;
+use App\Http\Controllers\ForpiSubmit;
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\MenuMiddleware;
 use App\Http\Controllers\ForpiMahasiswa;
 use App\Http\Controllers\ForpiController;
+use App\Http\Middleware\MenuMiddleware;
+use App\Http\Middleware\AdminOnlyMiddleware;
 
 Route::get('/', function () {
     return view('guest.landing');
@@ -16,14 +18,17 @@ Route::post('/portal/forpi', [Portal::class, 'forpi']);
 Route::get('/portal/logout', [Portal::class, 'logout'])->name('logout');
 
 Route::middleware([MenuMiddleware::class])->group(function () {
+    Route::get('/forpi/submit', [ForpiSubmit::class, 'index'])->name('forpi_submit');
     Route::get('/forpi', [ForpiController::class, 'index'])->name('forpi');
 
-    Route::get('/forpi/mahasiswa', [ForpiMahasiswa::class, 'index'])->name('forpi_mahasiswa');
-    Route::get('/forpi/mahasiswa/show/{nim}', [ForpiMahasiswa::class, 'show'])->name('forpi_mahasiswa_show');
-    Route::post('/forpi/mahasiswa/store', [ForpiMahasiswa::class, 'store'])->name('forpi_mahasiswa_store');
-    Route::post('/forpi/mahasiswa/import', [ForpiMahasiswa::class, 'import'])->name('forpi_mahasiswa_import');
-    Route::put('/forpi/mahasiswa/update/{nim}', [ForpiMahasiswa::class, 'update'])->name('forpi_mahasiswa_update');
-    Route::delete('/forpi/mahasiswa/destroy/{nim}', [ForpiMahasiswa::class, 'destroy'])->name('forpi_mahasiswa_destroy');
+    Route::middleware([AdminOnlyMiddleware::class])->group(function () {
+        Route::get('/forpi/mahasiswa', [ForpiMahasiswa::class, 'index'])->name('forpi_mahasiswa');
+        Route::get('/forpi/mahasiswa/show/{nim}', [ForpiMahasiswa::class, 'show'])->name('forpi_mahasiswa_show');
+        Route::post('/forpi/mahasiswa/store', [ForpiMahasiswa::class, 'store'])->name('forpi_mahasiswa_store');
+        Route::post('/forpi/mahasiswa/import', [ForpiMahasiswa::class, 'import'])->name('forpi_mahasiswa_import');
+        Route::put('/forpi/mahasiswa/update/{nim}', [ForpiMahasiswa::class, 'update'])->name('forpi_mahasiswa_update');
+        Route::delete('/forpi/mahasiswa/destroy/{nim}', [ForpiMahasiswa::class, 'destroy'])->name('forpi_mahasiswa_destroy');
 
-    Route::post('/forpi/pisn/import', [ForpiPisn::class, 'import'])->name('forpi_pisn_import');
+        Route::post('/forpi/pisn/import', [ForpiPisn::class, 'import'])->name('forpi_pisn_import');
+    });
 });
